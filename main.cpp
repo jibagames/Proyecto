1afc081567f74347c291b8988b5bfd65f15135f4
jibagames <80349363+jibagames@users.noreply.github.com>
2023-04-22 15:14:27 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,58 +1,57 @@
 
-#include<iostream>
-using namespace std;
-int main()
-{
-    int x;
-    cout<<"ingrese un numero"<<endl;
-    cin>>x;
-if((x<1)||(x>999)) cout<<"INGRESA UN NUMERO DEL 1 AL 999\n";
-else
- 
-    {
-        if(x>=900)   {cout<<"NOVECIENTOS " ;x=x-900;}
-       else if(x>=800)   {cout<<"OCHOCIENTOS " ;x=x-800;}
-       else if(x>=700)   {cout<<"SETECIENTOS " ;x=x-700;}
-       else if(x>=600)   {cout<<"SEISCIENTOS " ;x=x-600;}
-       else if(x>=500)   {cout<<"QUINIENTOS "  ;x=x-500;}
-       else if(x>=400)   {cout<<"CUATROCIENTOS "   ;x=x-400;}
-       else if(x>=300)   {cout<<"TRESCIENTOS " ;x=x-300;}
-       else if(x>=200)   {cout<<"DOSCIENTOS "  ;x=x-200;}
-       else if(x>100)    {cout<<"CIENTO "  ;x=x-100;}
-       else if(x==100)  {cout<<"CIEN"     ;x=x-100;}
-        if(x>90) {cout<<"NOVENTA Y "   ;x=x-90; }
-            if(x==90)   {cout<<"NOVENTA"  ;x=x-90; }  
-            if(x>80) {cout<<"OCHENTA Y "   ;x=x-80; }
-            if(x==80)   {cout<<"OCHENTA"  ;x=x-80; }
-            if(x>70) {cout<<"SETENTA Y "   ;x=x-70; }
-            if(x==70)   {cout<<"SETENTA"  ;x=x-70; }
-            if(x>60) {cout<<"SESENTA Y "   ;x=x-60; }
-            if(x==60)   {cout<<"SESENTA"  ;x=x-60; }
-            if(x>50) {cout<<"CINCUENTA Y " ;x=x-50; }
-            if(x==50)   {cout<<"CINCUENTA"    ;x=x-50; }
-            if(x>40) {cout<<"CUARENTA Y "  ;x=x-40; }
-            if(x==40)   {cout<<"CUARENTA" ;x=x-40; }
-            if(x>30) {cout<<"TREINTA Y "   ;x=x-30; }
-            if(x==30)   {cout<<"TREINTA"  ;x=x-30; }
-            if(x>20) {cout<<"VEINTI"       ;x=x-20; }
-            if(x==20)   {cout<<"VEINTE"       ;x=x-20; }
-        if(x>=16)    {cout<<"DIECI"        ;x=x-10; }
-       else if(x==15)   {cout<<"QUINCE"       ;x=x-15; }
-       else if(x==14)   {cout<<"CATORCE"  ;x=x-14; }
-       else if(x==13)   {cout<<"TRECE"        ;x=x-13; } 
-       else if(x==12)   {cout<<"DOCE"     ;x=x-12; }
-       else if(x==11)   {cout<<"ONCE"     ;x=x-11; }
-       else if(x==10)   {cout<<"DIEZ"     ;x=x-10; }      
-        if(x==9)    {cout<<"NUEVE"        ;x=x-9;  }
-        if(x==8)    {cout<<"OCHO"     ;x=x-8;  }
-        if(x==7)    {cout<<"SIETE"        ;x=x-7;  }
-            if(x==6)    {cout<<"SEIS"     ;x=x-6;  }
-       else if(x==5)    {cout<<"CINCO"        ;x=x-5;  }
-       else if(x==4)    {cout<<"CUATRO"       ;x=x-4;  }
-       else if(x==3)    {cout<<"TRES"     ;x=x-3;  }
-       else if(x==2)    {cout<<"DOS"      ;x=x-2;  }
-       else if(x==1)    {cout<<"UNO"      ;x=x-1;  }
-       }
-    cout<<endl;
-cin.ignore();return 0;
+package tienda2;
+
+
+import java.util.Scanner;
+
+public class Tienda2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+       
+        Producto[] products = {
+            new Laptop("Asus", 1000, "Intel Core i5"),
+            new Smartphone("iPhone12", 800, "iOS"),
+            new Tablet("iPad Air", 500, "10.9 inches"),
+            new TV("Samsung QLED", 1200, 55)
+        };
+
+        //mostrar productos disponibles
+        System.out.println("Productos disponibles en la tienda:");
+        for (Producto product : products) {
+            System.out.println(product.getName() + " - $" + product.getPrice());
+        }
+
+        // Cuanto es tu presupuesto
+        System.out.print("Ingresa tu presupuesto: $");
+        int presupuesto = scanner.nextInt();
+
+        // Productos que puedes comprar
+        System.out.println("Productos que puedes comprar:");
+        for (Producto product : products) {
+            if (product.getPrice() <= presupuesto) {
+                System.out.println(product.getName() + " - $" + product.getPrice());
+            }
+        }
+
+        // Cuales productos puedes comprar
+        System.out.print("Ingrese el nombre del producto que desea comprar: ");
+        String productName = scanner.next();
+
+        //Mirar si el producto esta disponible para comprar
+        boolean productoencontrar = false;
+        for (Producto product : products) {
+            if (productName.equalsIgnoreCase(product.getName())) {
+                if (product.getPrice() <= presupuesto) {
+                    System.out.println("Tu compraste " + product.getName() + " por el valor $" + product.getPrice() + "!");
+                    productoencontrar = true;
+                    break;
+                } else {
+                    System.out.println("Tu no puedes comprar esto " + product.getName() + "!");
+                    productoencontrar = false;
+                    break;
+                }
+            }
+        } 
+    }
 }
